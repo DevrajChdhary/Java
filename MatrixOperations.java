b307b74ba9e1fe48ff93c915648f42b2432494ed
Devraj Kohli <devrajkohli475@gmail.com>
2023-04-12 14:43:40 +0530

    Matrix Operation

---
+++ MatrixOperations.java
@@ -0,0 +1,66 @@
+import java.util.Scanner;
+
+public class MatrixOperations{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number of rows: ");
+        int rows = sc.nextInt();
+        System.out.print("Enter number of columns: ");
+        int columns = sc.nextInt();
+        
+        int[][] matrix1 = new int[rows][columns];
+        int[][] matrix2 = new int[rows][columns];
+        System.out.println("Enter the elements of the first matrix:");
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                matrix1[i][j] = sc.nextInt();
+            }
+        }
+        System.out.println("Enter the elements of the second matrix:");
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                matrix2[i][j] = sc.nextInt();
+            }
+        }
+        sc.close();
+        
+   
+        int[][] sumMatrix = new int[rows][columns];
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                sumMatrix[i][j] = matrix1[i][j] + matrix2[i][j];
+            }
+        }
+        System.out.println("Matrix Addition:");
+        displayMatrix(sumMatrix);
+       
+        int[][] diffMatrix = new int[rows][columns];
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                diffMatrix[i][j] = matrix1[i][j] - matrix2[i][j];
+            }
+        }
+        System.out.println("Matrix Subtraction:");
+        displayMatrix(diffMatrix);
+       
+        int[][] productMatrix = new int[rows][columns];
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                for (int k = 0; k < columns; k++) {
+                    productMatrix[i][j] += matrix1[i][k] * matrix2[k][j];
+                }
+            }
+        }
+        System.out.println("Matrix Multiplication:");
+        displayMatrix(productMatrix);
+    }
+    
+    public static void displayMatrix(int[][] matrix) {
+        for (int[] row : matrix) {
+            for (int element : row) {
+                System.out.print(element + " ");
+            }
+            System.out.println();
+        }
+    }
+}
